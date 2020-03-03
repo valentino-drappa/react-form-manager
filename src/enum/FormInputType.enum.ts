f055e13c671ee6898c7448c2be52974da82e2a93
vdrappa <valentino.drappa@contractor.voo.be>
2020-03-03 14:59:21 +0100

    fix input type checkbox name

--- src/enum/FormInputType.enum.ts
+++ src/enum/FormInputType.enum.ts
@@ -1,9 +1,9 @@
 export enum EFormInputType {
   INPUT_TYPE_TEXT = 'text',
   INPUT_TYPE_EMAIL = 'email',
-  INPUT_TYPE_CHECKBOX = 'checbox',
+  INPUT_TYPE_CHECKBOX = 'checkbox',
   INPUT_TYPE_RADIO = 'radio',
   INPUT_TYPE_PASSWORD = 'password',
   INPUT_TYPE_SELECT = 'select',
-  INPUT_TYPE_TEXT_AERA= 'textaera'
+  INPUT_TYPE_TEXT_AERA = 'textaera',
 }
